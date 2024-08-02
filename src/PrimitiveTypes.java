091c5b2722d5950f99629d70d3e10f239e6880b0
dinizfrontend <dinizfrontend@gmail.com>
2024-08-02 20:18:14 -0300

    feat: add primitive types

---
+++ src/PrimitiveTypes.java
@@ -0,0 +1,10 @@
+public class PrimitiveTypes {
+    boolean booleanVar = true;
+    byte byteVar = 127;
+    char charVar = 'F';
+    double doubleVar = 1000.2;
+    float floatVar = 10.2f;
+    int intVar = 10000;
+    long longVar = 100000;
+    short shortVar = 10000;
+}
